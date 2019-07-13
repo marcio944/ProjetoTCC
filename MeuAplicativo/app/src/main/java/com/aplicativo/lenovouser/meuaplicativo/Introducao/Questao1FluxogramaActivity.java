25f6092c839f305818e1fdb19760b4ea85cdf0cb
marcio944 <marcinho944@hotmail.com>
2019-07-13 00:37:36 -0300

    Adicionando alterações

--- MeuAplicativo/app/src/main/java/com/aplicativo/lenovouser/meuaplicativo/Introducao/Questao1FluxogramaActivity.java
+++ MeuAplicativo/app/src/main/java/com/aplicativo/lenovouser/meuaplicativo/Introducao/Questao1FluxogramaActivity.java
@@ -9,6 +9,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.aplicativo.lenovouser.meuaplicativo.MainActivity;
 import com.aplicativo.lenovouser.meuaplicativo.R;
 
 
@@ -23,12 +24,17 @@ public class Questao1FluxogramaActivity extends AppCompatActivity {
         radioButton = (RadioButton) findViewById(R.id.radioButton5);
     }
 
-    public void anteriorExemplo(View view){
+    public void inicio(View view){
+        Intent intent = new Intent(Questao1FluxogramaActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void anterior(View view){
         Intent intent = new Intent(Questao1FluxogramaActivity.this, ExemploFluxogramaActivity.class);
         startActivity(intent);
     }
 
-    public void questao2(View view){
+    public void proximo(View view){
         if(radioButton.isChecked()) {
             Toast.makeText(Questao1FluxogramaActivity.this, "Resposta correta!", Toast.LENGTH_LONG).show();
             Intent intent = new Intent(Questao1FluxogramaActivity.this, Questao2FluxogramaActivity.class);
