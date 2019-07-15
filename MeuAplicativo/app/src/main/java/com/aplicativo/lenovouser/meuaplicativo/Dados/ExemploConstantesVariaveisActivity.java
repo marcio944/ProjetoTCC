4896b36d9253be1c6007b449d2fe53c36f7d05ae
marcio944 <marcinho944@hotmail.com>
2019-07-15 00:20:41 -0300

    Adicionando alterações

--- MeuAplicativo/app/src/main/java/com/aplicativo/lenovouser/meuaplicativo/Dados/ExemploConstantesVariaveisActivity.java
+++ MeuAplicativo/app/src/main/java/com/aplicativo/lenovouser/meuaplicativo/Dados/ExemploConstantesVariaveisActivity.java
@@ -1,7 +1,9 @@
 package com.aplicativo.lenovouser.meuaplicativo.Dados;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.aplicativo.lenovouser.meuaplicativo.R;
 
@@ -12,4 +14,15 @@ public class ExemploConstantesVariaveisActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exemplo_constantes_variaveis);
     }
+
+    public void anterior(View view){
+        Intent intent = new Intent(ExemploConstantesVariaveisActivity.this, ConstantesVariaveisActivity.class);
+        startActivity(intent);
+    }
+
+    public void proximo(View view){
+        Intent intent = new Intent(ExemploConstantesVariaveisActivity.this, Questao1ConstantesVariaveisActivity.class);
+        startActivity(intent);
+    }
+
 }
